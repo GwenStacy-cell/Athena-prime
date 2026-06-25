1307722fdc8c47d18207ca9236ee6cfcbc3e2b40
hathi <hathimreigns1200@gmail.com>
2026-06-25 20:26:14 +0530

    style: change owner ping reaction emoji

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -414,7 +414,7 @@ export default {
     if (ownerId && userId !== ownerId && message.mentions.has(ownerId)) {
       try {
         // React to the message instantly
-        await message.react('1519681289957933066').catch(() => null);
+        await message.react('1519715561515384852').catch(() => null);
 
         // DM the owner
         const ownerUser = await message.client.users.fetch(ownerId).catch(() => null);
