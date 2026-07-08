31e68c4c7d07299eb66743cc73831309010edd47
hathi <hathimreigns1200@gmail.com>
2026-07-09 02:13:52 +0530

    style: make V2 container footer bold and grey

--- src/commands/utility.js
+++ src/commands/utility.js
@@ -566,7 +566,7 @@ function buildHelpContainer(client, guildId, moduleId = 'home') {
   rawComponents.push({ type: 14, divider: true });
   rawComponents.push(row2.toJSON());
   rawComponents.push({ type: 14, divider: true });
-  rawComponents.push({ type: 10, content: '-# Athena Prime Unbypassable Security !!' });
+  rawComponents.push({ type: 10, content: '-# **Athena Prime Unbypassable Security !!**' });
 
   // Raw Container JSON
   const rawContainer = {
