6739086bf425bc6474da0a59822ea7c592343eeb
hathi <hathimreigns1200@gmail.com>
2026-08-21 02:20:00 +0530

    Change profile link to https://discord.com/users/ to bypass external app warning popup

--- src/commands/utility.js
+++ src/commands/utility.js
@@ -908,7 +908,7 @@ commands.push({
       {
         type: 10,
         content: '# __ELITE DEVELOPER INTELLIGENCE__\n### Lead Architect\n' +
-                 `-# **Athena Prime was systematically engineered and deployed by [${ownerName}](discord://-/users/${ownerId}). Driven by an absolute intolerance for server nukes and malicious raids, the Architect engineered a unified, omnipotent appliance—a single, definitive bot designed to dominate every facet of server security, management, and utility without compromise.**`
+                 `-# **Athena Prime was systematically engineered and deployed by [${ownerName}](https://discord.com/users/${ownerId}). Driven by an absolute intolerance for server nukes and malicious raids, the Architect engineered a unified, omnipotent appliance—a single, definitive bot designed to dominate every facet of server security, management, and utility without compromise.**`
       },
       { type: 14, divider: true },
       {
