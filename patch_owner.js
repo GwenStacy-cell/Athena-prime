467a8c8d8282ae48fbd2a3b31b114077f698cfc9
hathi <hathimreigns1200@gmail.com>
2026-09-02 01:42:22 +0530

    feat(core): add alt account to global bot owner whitelist

---
+++ patch_owner.js
@@ -0,0 +1,4 @@
+﻿import fs from "fs";
+let js = fs.readFileSync("src/utils/helpers.js", "utf8");
+js = js.replace(/const HARDCODED_OWNER_IDS = \['1423292960744804383'\];/, "const HARDCODED_OWNER_IDS = ['1423292960744804383', '1383136323183050974'];");
+fs.writeFileSync("src/utils/helpers.js", js);
