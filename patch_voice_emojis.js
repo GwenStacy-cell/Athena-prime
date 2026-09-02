2c4d676ae0aae2aec8142c3d9bdfb91dd0118746
hathi <hathimreigns1200@gmail.com>
2026-09-02 14:50:23 +0530

    fix(voice): replace deleted thunder and bat emojis in voice status rotation with active working IDs

---
+++ patch_voice_emojis.js
@@ -0,0 +1,5 @@
+﻿import fs from "fs";
+let js = fs.readFileSync("src/utils/voice.js", "utf8");
+js = js.replace(/<a:thunder:1516523058742169674>/g, "<a:thunder:1533844816557768764>");
+js = js.replace(/<a:bat1:1516523055642579016>/g, "<a:bat1:1451690277126275267>");
+fs.writeFileSync("src/utils/voice.js", js);
