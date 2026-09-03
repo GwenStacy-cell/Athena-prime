ddd1d746e292e29264914f9d431e746545c28d90
hathi <hathimreigns1200@gmail.com>
2026-09-03 17:28:28 +0530

    docs(quote): add !quote setchannel to the interactive help menu utilities section

---
+++ patch_help_quote2.js
@@ -0,0 +1,9 @@
+﻿import fs from "fs";
+let js = fs.readFileSync("src/commands/utility.js", "utf8");
+
+js = js.replace(
+  "`!quote` `<msg_id|@user>` `[theme]` - Generate an aesthetic canvas quote `[public]`', '`!quotemaker` - Interactive canvas quote generator `[public]`'",
+  "`!quote` `<msg_id|@user>` `[theme]` - Generate an aesthetic canvas quote `[public]`', '`!quotemaker` - Interactive canvas quote generator `[public]`', '`!quote setchannel` `#channel` - Bind a dedicated Auto-Quote channel `[admin]`'"
+);
+
+fs.writeFileSync("src/commands/utility.js", js);
