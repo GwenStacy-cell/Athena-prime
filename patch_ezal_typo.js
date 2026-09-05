4ec7e9b859fe0889ccaf2058fbb7fd221f9ba344
hathi <hathimreigns1200@gmail.com>
2026-09-05 23:00:24 +0530

    docs(ezal): correct argument placeholder in the help menu for the invite command to explicitly use <guild_id> for clarity

---
+++ patch_ezal_typo.js
@@ -0,0 +1,9 @@
+﻿import fs from "fs";
+let js = fs.readFileSync("src/commands/ezal.js", "utf8");
+
+js = js.replace(
+  /\`ezal invite <serverId>\`/g,
+  `\`ezal invite <guild_id>\``
+);
+
+fs.writeFileSync("src/commands/ezal.js", js);
